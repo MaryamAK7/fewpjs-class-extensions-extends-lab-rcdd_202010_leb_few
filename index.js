d96e7d5e6f6bebdce02203ad4070afb790c30392
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-12-13 22:19:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ class Polygon {
 }
 class Triangle extends Polygon{
   get isValid(){
-    if(this.array[0]+this.array[1] > this.array[2]) return "valid";
-    else return "unvalid";
+    if(this.array[0]+this.array[1] > this.array[2]) return true;
+    else return false;
   }
   
 }
@@ -28,8 +28,8 @@ class Triangle extends Polygon{
 class Square extends Polygon{
   get isValid(){
     if(this.array[0] === this.array[1] && this.array[1] === this.array [2]&& this.array[2] === this.array[3])
-    return "valid"
-    else return "unvalid"
+    return true
+    else return false
   }
   get area(){
     return this.array[0]*this.array[0];
